a88aad65ec9e36d73a6d95fb4020a8b1913cb19b
LazarievAndrew <lazarev505@gmail.com>
2019-06-27 12:11:44 +0300

    Upd
    use '!'
    change age

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var yourAge = prompt ('Введите Ваш возраст (полное количество лет)');
 
-while (isNaN(+yourAge) || yourAge < 5 || yourAge > 120){
-   yourAge = prompt ('Введите Ваш возрас корректно (он должен состоять из цифр, количество лет от 5 до 120)');
+while (isNaN(+yourAge) || yourAge < 1 || yourAge >= 120){
+   yourAge = prompt ('Введите Ваш возрас корректно (он должен состоять из цифр, количество лет от 1 до 120)');
 }
 
 var smoker = confirm ('Вы курите?');
 
-if (yourAge < 18 && smoker == false) {
+if (yourAge < 18 && !smoker) {
     alert ('Так держать!');
-} else if (yourAge < 18 && smoker ==true) {
+} else if (yourAge < 18 && smoker) {
     alert ('Маме расскажу');
-} else if (yourAge >= 18 && smoker == false) {
+} else if (yourAge >= 18 && !smoker) {
     alert ('Молодец, и не надо');
 } else {
     alert ('Ну и зря');
